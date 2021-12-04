2d672042033aa31c1b2df8877a147cf5b91da779
hessah <hessah.mohammed.ruh.k@tuwaiq.edu.sa>
2021-12-04 17:44:47 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,20 @@
+
+
+
+
+fun main() {
+  val u1 = 3
+    val u2 = 50
+
+    addSum(u1,u2)
+
+
+}
+
+
+fun addSum (n1:Int,n2:Int){
+
+    val sum = n1 + n2
+    val sumNumber1 = sum+100
+    println("result = $sumNumber1")
+}
